6b81167289fb7bccdfe27065be5f3dfc3f4eb12f
Grzegorz Wilusz <grzegorzwilusz@Grzegorzs-MacBook-Pro.local>
2022-10-15 22:49:31 +0200

    Hide implementation of specific types behind the methods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -8,35 +8,40 @@ class GildedRose
     @items.each do |item|
       if sulfuras?(item)
       elsif generic?(item)
-        if item.quality > 0
-          decrease_quality(item)
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          if item.quality > 0
-            decrease_quality(item)
-          end
-        end
+        handle_generic(item)
       elsif aged_brie?(item)
-        if quality_less_than_50(item)
-          increase_quality(item)
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          if quality_less_than_50(item)
-            increase_quality(item)
-          end
-        end
+        handle_aged_brie(item)
       elsif backstage_pass?(item)
         handle_backstage_pass(item)
-        if item.sell_in < 0
-          item.quality = item.quality - item.quality
-        end
       end
     end
   end
 
   private
+
+  def handle_aged_brie(item)
+    if quality_less_than_50(item)
+      increase_quality(item)
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      if quality_less_than_50(item)
+        increase_quality(item)
+      end
+    end
+  end
+
+  def handle_generic(item)
+    if item.quality > 0
+      decrease_quality(item)
+    end
+    item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      if item.quality > 0
+        decrease_quality(item)
+      end
+    end
+  end
 
   def handle_backstage_pass(item)
     if quality_less_than_50(item)
@@ -53,6 +58,9 @@ class GildedRose
       end
     end
     item.sell_in = item.sell_in - 1
+    if item.sell_in < 0
+      item.quality = item.quality - item.quality
+    end
   end
 
   def generic?(item)
