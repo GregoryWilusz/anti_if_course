3864734fe31f27886f279fdbd634e1dba6bcf1c1
Grzegorz Wilusz <grzegorzwilusz@Grzegorzs-MacBook-Pro.local>
2022-10-17 21:52:16 +0200

    Building boundaries - Move goods classes to a separate namespace

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,4 @@
-class GildedRose
-
+module Inventory
   class Sulfuras
 
     attr_reader :quality, :sell_in
@@ -85,18 +84,20 @@ class GildedRose
       end
     end
   end
+end
 
+class GildedRose
   class GoodCategory
 
     def build_for(item)
       if sulfuras?(item)
-        Sulfuras.new(item.quality, item.sell_in)
+        Inventory::Sulfuras.new(item.quality, item.sell_in)
       elsif generic?(item)
-        Generic.new(item.quality, item.sell_in)
+        Inventory::Generic.new(item.quality, item.sell_in)
       elsif aged_brie?(item)
-        AgedBrie.new(item.quality, item.sell_in)
+        Inventory::AgedBrie.new(item.quality, item.sell_in)
       elsif backstage_pass?(item)
-        BackstagePass.new(item.quality, item.sell_in)
+        Inventory::BackstagePass.new(item.quality, item.sell_in)
       end
     end
 
